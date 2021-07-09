7584f52a80c5dd5070700eacf0765766967b2aa4
渡辺孝之 <na8esin.mpst@gmail.com>
2021-07-09 18:00:10 +0900

    もともとあったテストコードをkotlinに変更

---
+++ app/src/androidTest/java/com/example/android/testing/uiautomator/BasicSample/GooglePlayTest.kt
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2015, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.testing.uiautomator.BasicSample
+
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SdkSuppress
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiObject2
+import androidx.test.uiautomator.Until
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.CoreMatchers.`is` as Is
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.File
+
+
+/**
+ * Basic sample for unbundled UiAutomator.
+ */
+@RunWith(AndroidJUnit4::class)
+@SdkSuppress(minSdkVersion = 18)
+class GooglePlayTest {
+    companion object {
+        const val BASIC_SAMPLE_PACKAGE
+        = "com.example.android.testing.uiautomator.BasicSample"
+        const val LAUNCH_TIMEOUT:Long = 5000
+        const val STRING_TO_BE_TYPED = "UiAutomator";
+    }
+
+    private lateinit var mDevice: UiDevice
+
+    @Before
+    fun startMainActivityFromHomeScreen() {
+    
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+
+        // Start from the home screen
+        mDevice.pressHome()
+
+        // Wait for launcher
+        val launcherPackage = getLauncherPackageName()
+        assertThat(launcherPackage, notNullValue())
+        mDevice.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT)
+
+        // https://play.google.com/store
+
+        // Launch the blueprint app
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val intent = context.packageManager
+                .getLaunchIntentForPackage(BASIC_SAMPLE_PACKAGE);
+        intent?.addFlags(android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK);    // Clear out any previous instances
+        context.startActivity(intent);
+
+        // Wait for the app to appear
+        mDevice.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)), LAUNCH_TIMEOUT);
+    }
+
+    @Test
+    fun checkPreconditions() {
+        assertThat(mDevice, notNullValue());
+    }
+
+    @Test
+    fun testChangeText_sameActivity() {
+        // Type text and then press the button.
+        mDevice.findObject(By.res(BASIC_SAMPLE_PACKAGE, "editTextUserInput")).text =
+            STRING_TO_BE_TYPED;
+        mDevice.findObject(By.res(BASIC_SAMPLE_PACKAGE, "changeTextBt"))
+                .click();
+
+        // Verify the test is displayed in the Ui
+        val changedText: UiObject2 = mDevice
+                .wait(
+            Until.findObject(By.res(BASIC_SAMPLE_PACKAGE, "textToBeChanged")),
+                        500 /* wait 500ms */)
+        val file = File("/sdcard/Pictures/testChangeText_sameActivity.png")
+        val isTaken = mDevice.takeScreenshot(file)
+
+        assertThat(changedText.text, Is(equalTo(STRING_TO_BE_TYPED)))
+    }
+
+    /**
+     * Uses package manager to find the package name of the device launcher. Usually this package
+     * is "com.android.launcher" but can be different at times. This is a generic solution which
+     * works on all platforms.`
+     */
+    private fun getLauncherPackageName():String? {
+        // Create launcher Intent
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.addCategory(Intent.CATEGORY_HOME)
+
+        // Use PackageManager to get the launcher package name
+        val pm = ApplicationProvider.getApplicationContext<Context>().packageManager
+        val resolveInfo = pm.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY)
+        return resolveInfo?.activityInfo?.packageName
+    }
+}
